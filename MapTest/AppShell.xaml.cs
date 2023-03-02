752af6ca7107c2552d2201ec58326511469d8fe6
Paul Gee <paul.gee@nats.co.uk>
2023-03-02 09:09:38 +0000

    Updated MapTest

---
+++ MapTest/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MapTest;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ MapTest/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
+
+namespace MapTest;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+
+        var pinLocation = new Location(20.7557, -155.9880);
+
+        MapSpan mapSpan = MapSpan.FromCenterAndRadius(pinLocation, Distance.FromKilometers(3));
+        map.MoveToRegion(mapSpan);
+        map.Pins.Add(new Pin
+        {
+            Label = "MAUI Test Pin",
+            Location = pinLocation
+        });
+    }
+    async void OnMapClicked(object sender, MapClickedEventArgs e)
+    {
+        await DisplayAlert("Map", $"Area {e.Location.Latitude}, {e.Location.Longitude} clicked.", "Ok");
+    }
+}
